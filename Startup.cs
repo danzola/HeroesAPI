c173a6aac01e910a674184682db99cf27fcbcf1f
Diego Anzola <diego_anzola@yahoo.es>
2020-02-25 21:19:52 +0000

    View errors

--- Startup.cs
+++ Startup.cs
@@ -34,11 +34,7 @@ namespace HeroesAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseDeveloperExceptionPage();        
             app.UseRouting();
             app.UseAuthorization();
 
