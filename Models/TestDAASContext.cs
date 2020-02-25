95f87559610a0dcd9cac63a7446582e4fbd8a3e7
Diego Anzola <diego_anzola@yahoo.es>
2020-02-25 14:11:39 +0000

    New API

---
+++ Models/TestDAASContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace HeroesAPI.Models
+{
+    public partial class TestDAASContext : DbContext
+    {
+        public TestDAASContext()
+        {
+        }
+
+        public TestDAASContext(DbContextOptions<TestDAASContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Heroes> Heroes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Heroes>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Hero)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
